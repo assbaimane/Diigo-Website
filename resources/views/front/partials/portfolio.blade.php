b0a3347e6b909bedabe3d542dceacdd5f0f722e4
Imane <assbaimane@hotmail.com>
2022-03-11 09:57:39 +0100

    Bug : trying to fix untracked files

--- resources/views/front/partials/portfolio.blade.php
+++ resources/views/front/partials/portfolio.blade.php
@@ -1,32 +1,37 @@
-      <!-- Projects -->
-      <div class="projects">
-        <div class="container">
-           <div class="row">
-              <div class="col-md-12">
-                 <div class="titlepage">
+<!-- Projects -->
+<div class="projects">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="titlepage">
                     <span>Previous Projects</span>
                     <h2>Better position of Business</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page </p>
-                 </div>
-              </div>
-           </div>
-           <div class="row">
-              <div class="col-md-10 offset-md-1">
-                 <div class="row">
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a
+                        page </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <div class="row">
                     <div class="col-md-6 offset-md-3">
-                       <div class="projects_box ">
-                          <figure><img src="images/projects_img.png" alt="#"/></figure>
-                       </div>
+                        <div class="projects_box ">
+                            <figure><img src="images/projects_img.png" alt="#" /></figure>
+                        </div>
                     </div>
                     <div class="col-md-12">
-                       <div class="projects_box ">
-                          <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believableThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
-                          <a class="read_more" href="#">Read more</a>
-                       </div>
+                        <div class="projects_box ">
+                            <p>There are many variations of passages of Lorem Ipsum available, but the majority
+                                have suffered alteration in some form, by injected humour, or randomised words
+                                which don't look even slightly believableThere are many variations of passages of
+                                Lorem Ipsum available, but the majority have suffered alteration in some form, by
+                                injected humour, or randomised words which don't look even slightly believable</p>
+                            <a class="read_more" href="#">Read more</a>
+                        </div>
                     </div>
-                 </div>
-              </div>
-           </div>
+                </div>
+            </div>
         </div>
-     </div>
-     <!-- end projects -->
+    </div>
+</div>
+<!-- end projects -->
